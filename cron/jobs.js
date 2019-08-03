5d42de6d120008b1404e0ccaa72f513258b513d3
Dmytro Ivashchuk <dimaiv@me.com>
2019-08-03 18:16:04 +0200

    allow for passing complex object to rabbit mq

--- cron/jobs.js
+++ cron/jobs.js
@@ -4,7 +4,10 @@ const q = "task";
 
 const runStats = async ({ done }) => {
   const registeredUsers = await User.find();
-  const listOfTwitterIds = registeredUsers.map(v => v.twitterHandler);
+  const listOfTwitterIds = registeredUsers.map(v => ({
+    twitterHandler: v.twitterHandler,
+    userMongoId: v._id
+  }));
   listOfTwitterIds.forEach(v =>
     open
       .then(function(conn) {
@@ -12,7 +15,7 @@ const runStats = async ({ done }) => {
       })
       .then(function(ch) {
         return ch.assertQueue(q).then(function(ok) {
-          return ch.sendToQueue(q, Buffer.from(v));
+          return ch.sendToQueue(q, Buffer.from(JSON.stringify(v)));
         });
       })
       .catch(console.warn)
